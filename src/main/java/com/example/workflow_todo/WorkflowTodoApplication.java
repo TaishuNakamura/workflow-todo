d95f659d1f2380e05fda25aad2589afed62c99d5
TaishuNakamura <tai.empty-border.172935-1026@docomo.ne.jp>
2026-02-11 17:56:16 +0900

    Springbootプロジェクトの追加

---
+++ src/main/java/com/example/workflow_todo/WorkflowTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.workflow_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowTodoApplication.class, args);
+	}
+
+}
